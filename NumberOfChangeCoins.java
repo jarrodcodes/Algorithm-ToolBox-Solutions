1f653d34f664b3ac1cb9bffced98a4619792c8f6
Jarrod Young <jarrodsy@gmail.com>
2018-06-23 19:13:01 -0400

    Create NumberOfChangeCoins.java

---
+++ NumberOfChangeCoins.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NumberOfChangeCoins {
+
+    private static long calc_fib(int n) {
+
+        long numQuarters = 0;
+        long numDimes = 0;
+        long numNickels = 0;
+        long numPennies = 0;
+
+        while (n >= 1) {
+
+            if (n >= 25) {
+                n -= 25;
+                numQuarters += 1;
+            } else if (n > 10 && n <= 24) {
+                n -= 10;
+                numDimes += 1;
+            } else if (n <= 9 && n > 4) {
+                n -= 5;
+                numNickels += 1;
+            } else if (n <= 4) {
+                n -= 1;
+                numPennies += 1;
+            }
+        }
+
+        return (numQuarters + numDimes + numNickels + numPennies);
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(calc_fib(n));
+    }
+}

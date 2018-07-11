59e06b150ac5e6681e7eeb1157def7c6ffb55d51
Jarrod Young <jarrodsy@gmail.com>
2018-07-11 18:16:29 -0400

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BinarySearch {
+
+    static int binarySearch(int[] array, int target) {
+
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+
+            int middle = left + (right - left) / 2;
+
+            if (array[middle] == target) {
+
+                return middle;
+            }
+
+            if (array[middle] < target) {
+
+                left = middle + 1;
+            }
+
+            else {
+
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            //replace with the call to binarySearch when implemented
+            System.out.print(binarySearch(a, b[i]) + " ");
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
